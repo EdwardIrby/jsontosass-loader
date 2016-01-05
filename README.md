d8787e1b3ab3038b5567ef49f45bdadbbb660ff9
Edward Irby <edward@castersblues.com>
2016-01-05 02:20:48 -0800

    marked depencies for and flagged cahcehable

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # json to sass loader for webpack
 
-## Installation
+### 0.1.3 Changes
+- **Simpler implementation** (See Example Config)
+- **Flagged as cacheable**
+- **Marked dependencies**
+
+
+### Installation
 
 `npm install jsontosass-loader --save-dev`
 
@@ -9,15 +15,14 @@
 [Documentation: Using loaders](http://webpack.github.io/docs/using-loaders.html)
 
 ### Example config
+**You can use either a json file or a js file with the modules.export syntax as your vars file.**
 
 ``` javascript
-var sassGlobals = require('[YourVars.json file]');
-var sassVars = JSON.stringify(sassGlobals);
-// => returns [YourVars.json file] content as string
+var sassVars = 'path/to/your/vars.js|json';
 var webpackConfig = {
     module: {
         loaders:[
-            {test: /.scss$/, loader: "style!css!sass!jsontosass?" + sassVars}
+            {test: /.scss$/, loader: "style!css!sass!jsontosass?path="+ sassVars}
         ]
     },
 }
@@ -41,19 +46,6 @@ var webpackConfig = {
 $breakpoints:(portraitS:320px,portraitM:360px,portraitL:414px);
 $localNavHeight:50px;
 ```
-
-### CSS Modules config
-
-````js
-
-{ test: /\.scss$/, loader: ExtractTextPlugin.extract(
-        "style-loader",
-        "css-loader?minimize&sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]"+
-        "!postcss-loader" +
-        "!sass-loader?sourceMap"+
-        "!jsontosass?"+ sassVars) 
-},
-````
 
 
 Forked from gist: [jsonToSassVars](https://gist.github.com/Kasu/ea4f4861a81e626ea308) and [prepend-loader](https://gist.github.com/Kasu/29452051023ff5337bd7)
--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 "use strict";
 
 var loaderUtils = require("loader-utils");
-
+var fs = require('fs');
+var path = require("path");
 module.exports = function(content) {
+  var query = loaderUtils.parseQuery(this.query).path;
+  var queryString = JSON.stringify(query);
+  var varPath = queryString.replace(/["']/g, '');
+  this.cacheable();
+  var contentPath = path.resolve(varPath);
+  this.addDependency(contentPath);
+  var obj = JSON.parse(fs.readFileSync(contentPath, 'utf8'));
 
 
-
-    function jsonToSassVars (obj, indent) {
-        // Make object root properties into sass variables
-        var sass = "";
-        for (var key in obj) {
-            sass += "$" + key + ":" + JSON.stringify(obj[key], null, indent) + ";\n";
-        }
-
-        // Store string values (so they remain unaffected)
-        var storedStrings = [];
-        sass = sass.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, function (str) {
-
-            var id = "___JTS" + storedStrings.length;
-            storedStrings.push({id: id, value: str});
-            return id;
-        });
-
-        // Convert js lists and objects into sass lists and maps
-        sass = sass.replace(/[{\[]/g, "(").replace(/[}\]]/g, ")");
-
-        // Put string values back (now that we're done converting)
-        storedStrings.forEach(function (str) {
-            str.value = str.value.replace(/["']/g, '');
-            sass = sass.replace(str.id, str.value);
-        });
-
-        return sass;
+  function jsonToSassVars (obj, indent) {
+    // Make object root properties into sass variables
+    var sass = "";
+    for (var key in obj) {
+      sass += "$" + key + ":" + JSON.stringify(obj[key], null, indent) + ";\n";
     }
 
-    var query = loaderUtils.parseQuery(this.query);
+    // Store string values (so they remain unaffected)
+    var storedStrings = [];
+    sass = sass.replace(/(["'])(?:(?=(\\?))\2.)*?\1/g, function (str) {
+
+      var id = "___JTS" + storedStrings.length;
+      storedStrings.push({id: id, value: str});
+      return id;
+    });
+
+    // Convert js lists and objects into sass lists and maps
+    sass = sass.replace(/[{\[]/g, "(").replace(/[}\]]/g, ")");
+
+    // Put string values back (now that we're done converting)
+    storedStrings.forEach(function (str) {
+      str.value = str.value.replace(/["']/g, '');
+      sass = sass.replace(str.id, str.value);
+    });
+
+    return sass;
+  }
 
 
+  var sass = jsonToSassVars(obj);
 
-    var sass = jsonToSassVars(query);
-
-    return sass ? sass + '\n ' + content : content;
+  return sass ? sass + '\n ' + content : content;
 }
