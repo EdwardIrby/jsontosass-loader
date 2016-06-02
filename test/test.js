71c430d6abf8f0fafcb611ced66179174b9d1eba
Rafael Eyng <rafaeleyng@gmail.com>
2016-06-02 01:47:16 -0300

    cleanup test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@ var should = require('should');
 require('should-sinon');
 
 var loader = require('../');
-var query = '?path=test/test.json';
 var context;
 var someSass = '$primary-color: #333;'
 
@@ -15,7 +14,7 @@ describe('loader', function() {
     }
   });
 
-  it('should convert JS numbers to Sass', function() {
+  it('should convert JSON numbers to Sass', function() {
     context.query = '?path=test/json/numbers.json'
     loader.call(context, someSass)
       .should.be.eql('$zIndex:100;\n$opacity:0.5;\n\n$primary-color: #333;');
@@ -24,7 +23,7 @@ describe('loader', function() {
     context.addDependency.should.have.callCount(1)
   });
 
-  it('should convert JS strings to Sass', function() {
+  it('should convert JSON strings to Sass', function() {
     context.query = '?path=test/json/strings.json'
     loader.call(context, someSass)
       .should.be.eql('$localNavHeight:50px;\n$fontSize:16px;\n\n$primary-color: #333;');
@@ -33,7 +32,7 @@ describe('loader', function() {
     context.addDependency.should.have.callCount(1)
   });
 
-  it('should convert JS lists to Sass lists', function() {
+  it('should convert JSON lists to Sass lists', function() {
     context.query = '?path=test/json/lists.json'
     loader.call(context, someSass)
       .should.be.eql('$defaultMargin:(10px,0,5px,0);\n$deepList:((1,2,3),(4,5,6));\n\n$primary-color: #333;');
@@ -42,7 +41,7 @@ describe('loader', function() {
     context.addDependency.should.have.callCount(1)
   });
 
-  it('should convert JS objects to Sass maps', function() {
+  it('should convert JSON objects to Sass maps', function() {
     context.query = '?path=test/json/objects.json'
     loader.call(context, someSass)
       .should.be.eql('$breakpoints:(portraitS:320px,portraitM:360px,portraitL:414px);\n$deepObject:(a:(b:c));\n\n$primary-color: #333;');
